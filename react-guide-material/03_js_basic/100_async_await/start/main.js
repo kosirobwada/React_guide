24413a9a059603aa8502bde10a7fae8648e9e2a0
kosirobwada <kosiro131219@gmail.com>
2024-05-23 02:17:57 +0900

    js-basic

--- react-guide-material/03_js_basic/100_async_await/start/main.js
+++ react-guide-material/03_js_basic/100_async_await/start/main.js
@@ -1,17 +1,23 @@
 // 非同期処理（Promise）
 let a = 0;
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        a = 1;
-        resolve(a)
-    }, 2000);
-}).then((b) => {
-    console.log(b);
-    return b;
-}).then((b) => {
-    console.log(b);
-}).catch((c) => {
-    console.log('catchが実行', c)
-})
+async function init() {
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            a = 1;
+            resolve(a)
+        }, 2000);
+    })
 
+    console.log(a);
+    // }).then((b) => {
+    //     console.log(b);
+    //     return b;
+    // }).then((b) => {
+    //     console.log(b);
+    // }).catch((c) => {
+    //     console.log('catchが実行', c)
+    // })
+    
+    
+}
